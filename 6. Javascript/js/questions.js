85a0aa7964b292b14071370fc698bcc8e8d32783
Apostolos Siokas <apostolossiokas@gmail.com>
2021-02-27 11:28:34 +0200

    questions game

---
+++ 6. Javascript/js/questions.js
@@ -0,0 +1,14 @@
+var questions = [
+    {
+        "question": "Ο Bruce Wane είναι ο Superman",
+        "answer": false
+    },
+    {
+        "question": "Ο Spiderman είναι αράχνη",
+        "answer": true
+    },
+    {
+        "question": "To pikatchu πετάει φωτιές",
+        "answer": false
+    },
+];
